4eb1255ae8ea407d3e1cc86886fc8386fe40e938
Rituparna W <rituparnaw16@gmail.com>
2025-10-26 10:48:12 +0000

    chore: organize project
    TODO: create Makefile instead of build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+aarch64-linux-gnu-as src/boot.S -o build/boot.o
+aarch64-linux-gnu-gcc -ffreestanding -c src/kernel.c -o build/kernel.o
+aarch64-linux-gnu-ld -nostdlib -T linker.ld build/boot.o build/kernel.o -o build/kernel.elf
+
+qemu-system-aarch64 -machine virt -nographic -cpu cortex-a72 -kernel build/kernel.elf
+# qemu-system-aarch64 -machine virt,virtualization=on,secure=on -nographic -cpu cortex-a72 -kernel build/kernel.elf
