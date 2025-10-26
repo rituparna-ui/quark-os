8c3de5cbe2088b746275af8d7cbd0b3f090d315f
Rituparna W <rituparnaw16@gmail.com>
2025-10-26 13:27:10 +0000

    feat(exception-level): fetch and print current excpetion level
    Tested with virtualization and secure flags

---
+++ src/lib/utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef LIB_UTILS_H
+#define LIB_UTILS_H
+
+#include <stdint.h>
+
+uint32_t get_current_el(void);
+char *get_el_name(uint32_t el);
+void print_current_el_name(void);
+
+#endif // LIB_UTILS_H
