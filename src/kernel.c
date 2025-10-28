2b06c0fb534c2422a6f4a89324e748c2788a6a65
Rituparna W <rituparnaw16@gmail.com>
2025-10-28 23:52:33 +0000

    kek

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,14 @@
 #include "lib/uart/uart.h"
 #include "lib/utils/utils.h"
 
+void _setup_vbar(void);
+
 void kernel_main() {
   uart_init();
 
   print_current_el_name();
-
-  uart_put_uint(81283924);
+  uart_puts("Setting vector table");
+  _setup_vbar();
 
   while (1) {
   }
