7a02add55eb8557dcd03eb3f35d16efd4f82c023
Rituparna W <rituparnaw16@gmail.com>
2025-11-21 03:40:56 +0100

    feat(exception-levels): Fetch and print current exception levels
    Tested all exception levels by toggling virtualization and secure monitor flags.
    
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

---
+++ src/lib/utils/utils.h
@@ -0,0 +1,11 @@
+#ifndef LIB_UTILS_H
+#define LIB_UTILS_H
+
+#include "../uart/uart.h"
+#include <stdint.h>
+
+// uint32_t get_current_el_number();
+// char* get_el_name(uint32_t el);
+void print_current_el();
+
+#endif // !LIB_UTILS_H
