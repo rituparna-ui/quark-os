8b8864005217deb95b24dc94bc61b188f666ecdb
Rituparna W <rituparnaw16@gmail.com>
2025-11-21 01:42:52 +0100

    feat(UART): implement proper UART hardware initialization and data FIFO queue
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,12 @@
 #include "lib/uart/uart.h"
 
 void kernel_main() {
+  uart_init();
+
   char *str = "Hello, World !!!";
-  uart_puts(str);
+  uart_println(str);
 
   while (1) {
+    uart_putc(uart_getc());
   }
 }
