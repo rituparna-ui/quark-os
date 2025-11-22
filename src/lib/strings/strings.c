960917aa49a30faf7f0fc3c010fa50873fe23eff
Rituparna W <rituparnaw16@gmail.com>
2025-11-22 07:21:01 +0100

    feat(string): Fix hex convert to add 0x as prefix and new function to convert uint32 to decimal string
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/lib/strings/strings.c
+++ src/lib/strings/strings.c
@@ -3,21 +3,61 @@
 
 char *uint32_to_hex_string(uint32_t value) {
   const int num_digits = 8;
-  char *buffer = (char *)simple_heap_alloc(8);
+  const int prefix_len = 2;
+  const int total_len = prefix_len + num_digits;
+
+  char *buffer = (char *)simple_heap_alloc(total_len + 1);
+  if (!buffer) {
+    return "ERR";
+  }
+
+  buffer[0] = '0';
+  buffer[1] = 'x';
 
   for (int i = 0; i < num_digits; i++) {
     int shift = (num_digits - 1 - i) * 4;
+    uint32_t digit = (value >> shift) & 0xF;
 
-    uint32_t digit_value = (value >> shift) & 0xF;
+    if (digit < 10) {
 
-    if (digit_value < 10) {
-      buffer[i] = digit_value + '0';
+      buffer[prefix_len + i] = '0' + digit;
     } else {
-      buffer[i] = digit_value + 'A' - 10;
+      buffer[prefix_len + i] = 'A' + (digit - 10);
     }
   }
 
-  buffer[num_digits] = '\0';
+  buffer[total_len] = '\0';
+  return buffer;
+}
+
+char *uint32_to_dec_string(uint32_t value) {
+  const int max_digits = 10;
+
+  char *buffer = (char *)simple_heap_alloc(max_digits + 1);
+  if (!buffer) {
+    return "ERR";
+  }
+
+  if (value == 0) {
+    buffer[0] = '0';
+    buffer[1] = '\0';
+    return buffer;
+  }
+
+  int pos = max_digits;
+  buffer[pos] = '\0';
+
+  while (value > 0) {
+    uint32_t digit = value % 10;
+    value /= 10;
+    buffer[--pos] = '0' + digit;
+  }
+
+  int start = 0;
+  while (buffer[pos]) {
+    buffer[start++] = buffer[pos++];
+  }
+  buffer[start] = '\0';
 
   return buffer;
 }
--- src/lib/strings/strings.h
+++ src/lib/strings/strings.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 char *uint32_to_hex_string(uint32_t value);
+char *uint32_to_dec_string(uint32_t value);
 
 #endif // !LIB_STRINGS_H
