c4ecd833068089615cdffb6d06d52373039f741e
Rituparna W <rituparnaw16@gmail.com>
2026-02-17 19:20:19 +0100

    chore: reorganize project
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
 # -g for gdb debugging symbols
-aarch64-linux-gnu-as -g boot.S -o boot.o
+aarch64-linux-gnu-as -g src/boot.S -o build/boot.o
 
 aarch64-linux-gnu-gcc -ffreestanding -g \
   -nostdlib -nostartfiles \
   -Wall -Wextra -O0 -mstrict-align \
-  -c kernel.c -o kernel.o
+  -c src/kernel.c -o build/kernel.o
 
-aarch64-linux-gnu-ld -nostdlib -g -T linker.ld boot.o kernel.o -o kernel.elf
+aarch64-linux-gnu-ld -nostdlib -g -T linker.ld build/boot.o build/kernel.o -o build/kernel.elf
 
-qemu-system-aarch64 -machine virt -nographic -cpu cortex-a72 -kernel kernel.elf -s -S
+qemu-system-aarch64 -machine virt -nographic -cpu cortex-a72 -kernel build/kernel.elf -s -S
 # gdb-multiarch ./kernel.elf -ex "target remote :1234" -ex "layout split"
 
