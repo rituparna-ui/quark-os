29ecc8c212f9e95c4bc053b198351a756e841e86
Rituparna W <rituparnaw16@gmail.com>
2026-02-18 01:13:12 +0100

    feat(uart): output chars to uart
    Write directly to the UART Data Register without any hardware checks
    
    Signed-off-by: Rituparna W <rituparnaw16@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,15 @@
+#include <stdint.h>
+
+#define UART_BASE 0x09000000
+#define UART_DR ((volatile uint32_t *)(UART_BASE + 0x00))
+
 void kernel_main() {
+  *UART_DR = 'H';
+  *UART_DR = 'e';
+  *UART_DR = 'l';
+  *UART_DR = 'l';
+  *UART_DR = 'o';
+
   while (1) {
   }
 }
