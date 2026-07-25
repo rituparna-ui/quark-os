6103d2f02cb5f6af03b475c6e6736f4121dd0dd7
Rituparna W <rituparnaw16@gmail.com>
2026-07-25 08:20:00 +0000

    feat(uart): output characters directly to UART data register

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,15 @@
+#include <stdint.h>
+
+#define UART_BASE 0x9000000UL
+#define UART_DR (UART_BASE + 0x00)
+
 void kernel_main() {
+  char *str = "Hello, World !";
+
+  while (*str) {
+    *(volatile uint32_t *)UART_BASE = *str++;
+  }
+
   while (1) {
   }
 }
